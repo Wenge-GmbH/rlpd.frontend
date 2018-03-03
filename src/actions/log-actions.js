6794d385ec2cca47e8999cbc962663f1db3a5a22
Markus Maelzer <markus.maelzer@gmail.com>
2018-03-03 18:33:41 +0100

    .

--- src/actions/log-actions.js
+++ src/actions/log-actions.js
@@ -5,13 +5,13 @@ const storage = firebase.storage();
 const db = firebase.database();
 
 export const getImgUrl = (path) => {
-  storage.ref(path)
+  storage.ref(path);
 }
 
 export const getLog = (log) => {
   return dispatch => {
     console.log(`logs/${log}`);
-    db.ref(`logs/${log}`).on('value', snapshot => {
+    db.ref(`logs/${log}`).orderByKey().limitToLast(50).on('value', snapshot => {
       if(log === 'success') {
         console.log(snapshot.val());
         dispatch({
--- src/components/log/success-log.js
+++ src/components/log/success-log.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 import { getLog } from '../../actions';
 
 class SuccessLog extends Component {
@@ -7,10 +8,23 @@ class SuccessLog extends Component {
     this.props.getLog('success');
   }
   render() {
+    const log = _.map(this.props.success, (item) => {
+      return (
+        <div key={item.epoch_time}>
+          <p>{item.epoch_time}</p>
+          <p>{item.plate}</p>
+          <p>{item.processing_time_ms}</p>
+        </div>
+      );
+    })
     return (
-      <div></div>
+      <div>{log}</div>
     );
   }
 }
 
-export default connect(null, { getLog })(SuccessLog);
+const mapStateToProps = ({log: { success }}) => ({
+  success
+})
+
+export default connect(mapStateToProps, { getLog })(SuccessLog);
